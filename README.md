dbf6e24213f37fa0e6aacaaffdbd00632dd3c998
Dongkyung <suzxc2468@gmail.com>
2023-07-13 13:56:18 +0900

    Webtoon 앱 개발완료

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+# 프로젝트 시작하기
+main 함수에 직접 start / debug 선택.
+
+# 프로젝트 빌드하기
+아래 명령어 실행 
+flutter build apk --release --target-platform=android-arm64
+project root > build > app > outputs > apk 경로에서 파일 확인
+
--- lib/webtoon/services/api_service.dart
+++ lib/webtoon/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/webtoon/models/webtoon_detail_model.dart';
+import 'package:toonflix/webtoon/models/webtoon_episode_model.dart';
 import 'package:toonflix/webtoon/models/webtoon_model.dart';
 
 class ApiService {
@@ -18,6 +20,31 @@ class ApiService {
         webtoonInstances.add(toon);
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatesEpisodeById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
